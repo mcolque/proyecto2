d6411468621023947edb085fb3c2223370b1c4bd
unknown <matiascolque.mc@gmail.com>
2022-04-04 20:16:45 -0300

    actualizacion

--- src/main/java/tec/colmena/proyecto/controllers/HelloWord.java
+++ src/main/java/tec/colmena/proyecto/controllers/HelloWord.java
@@ -1,15 +1,31 @@
 package tec.colmena.proyecto.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloWord {
 
+	@Autowired
+	private RestTemplate clienteRest;
 	
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Bienvenido a Heroku";
+	}
+	
+	@GetMapping("/sumar")
+	public String sumar(@RequestParam float a, @RequestParam float b) {
+		String resultado = Float.toString(a+b);
+		return clienteRest.getForObject("https://resultadosproject.herokuapp.com/resultado/"+resultado, String.class);
+	}
+	
+	@GetMapping("/restar")
+	public float restar(@RequestParam float a, @RequestParam float b) {
+		return (a - b);
 	}
 	
 	
